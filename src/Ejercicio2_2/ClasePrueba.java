8b935c825d7a099379de10e859a2fa5bf2b8eab6
JesusAguilarUDC <jaguilarn1@unicartagena.edu.co>
2024-11-21 23:47:15 -0500

    Se agrego el ejercicio 2.2 y 3.

---
+++ src/Ejercicio2_2/ClasePrueba.java
@@ -0,0 +1,16 @@
+
+package Ejercicio2_2;
+
+import Ejercicio2.Moto;
+
+public class ClasePrueba {
+    public static void main(String[] args) {
+        Moto moto = new Moto("Deportiva");
+
+        moto.mostrarMoto();
+
+     //  moto.mostrarVehiculo();
+
+    }
+}
+
